2ae705f25172e27e0d2f99d175e1b9dd40844828
aamathew <134253292+aamathew@users.noreply.github.com>
2023-07-10 12:16:56 -0400

    part4?

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+SESSIONS = (
+    ('A', 'Aerobics'),
+    ('B', 'Boxing'),
+    ('C', 'Cardio'),
+    ('P', 'Pilates'),
+    ('S', 'Swimming'),
+    ('W', 'Weightlifting'),
+    ('Y', 'Yoga')
+)
+session = models.CharField(max_length=20, choices=SESSIONS)
+
 
 class Exercise(models.Model):
     name = models.CharField(max_length=100)
@@ -15,3 +26,19 @@ class Exercise(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'exercise_id': self.id})
+
+class Workout(models.Model):
+    date = models.DateField()
+    session = models.CharField(
+        max_length=1,
+        choices=SESSIONS,
+        default='SESSIONS'[0][0]
+    )
+    # Create a exercise_id ForeignKey
+    exercise = models.ForeignKey(
+        Exercise,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.get_session_display()} on {self.date}"
